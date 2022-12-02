272571d8080c2f7480d45a84b77c5a3e84ea2654
Eric Dasse <christ.gououi@gmail.com>
2022-12-02 16:29:15 +0100

    Improve deck test readability

--- test_card_games.py
+++ test_card_games.py
@@ -2,6 +2,7 @@ import pytest
 
 from card import Card, Rank, Suit
 from deck import Deck
+from helpers import LIST_OF_52_CARDS
 
 
 @pytest.mark.parametrize(
@@ -21,64 +22,7 @@ def test_can_create_a_card(suit, rank):
 
 
 def test_can_create_52_cards_deck():
-    list_52_cards = [
-        # Spade
-        Card(suit=Suit.SPADE, rank=Rank.ACE),
-        Card(suit=Suit.SPADE, rank=Rank.TWO),
-        Card(suit=Suit.SPADE, rank=Rank.THREE),
-        Card(suit=Suit.SPADE, rank=Rank.FOUR),
-        Card(suit=Suit.SPADE, rank=Rank.FIVE),
-        Card(suit=Suit.SPADE, rank=Rank.SIX),
-        Card(suit=Suit.SPADE, rank=Rank.SEVEN),
-        Card(suit=Suit.SPADE, rank=Rank.EIGHT),
-        Card(suit=Suit.SPADE, rank=Rank.NINE),
-        Card(suit=Suit.SPADE, rank=Rank.TEN),
-        Card(suit=Suit.SPADE, rank=Rank.KING),
-        Card(suit=Suit.SPADE, rank=Rank.QUEEN),
-        Card(suit=Suit.SPADE, rank=Rank.JACK),
-        # Heart
-        Card(suit=Suit.HEART, rank=Rank.ACE),
-        Card(suit=Suit.HEART, rank=Rank.TWO),
-        Card(suit=Suit.HEART, rank=Rank.THREE),
-        Card(suit=Suit.HEART, rank=Rank.FOUR),
-        Card(suit=Suit.HEART, rank=Rank.FIVE),
-        Card(suit=Suit.HEART, rank=Rank.SIX),
-        Card(suit=Suit.HEART, rank=Rank.SEVEN),
-        Card(suit=Suit.HEART, rank=Rank.EIGHT),
-        Card(suit=Suit.HEART, rank=Rank.NINE),
-        Card(suit=Suit.HEART, rank=Rank.TEN),
-        Card(suit=Suit.HEART, rank=Rank.KING),
-        Card(suit=Suit.HEART, rank=Rank.QUEEN),
-        Card(suit=Suit.HEART, rank=Rank.JACK),
-        # Diamond
-        Card(suit=Suit.DIAMOND, rank=Rank.ACE),
-        Card(suit=Suit.DIAMOND, rank=Rank.TWO),
-        Card(suit=Suit.DIAMOND, rank=Rank.THREE),
-        Card(suit=Suit.DIAMOND, rank=Rank.FOUR),
-        Card(suit=Suit.DIAMOND, rank=Rank.FIVE),
-        Card(suit=Suit.DIAMOND, rank=Rank.SIX),
-        Card(suit=Suit.DIAMOND, rank=Rank.SEVEN),
-        Card(suit=Suit.DIAMOND, rank=Rank.EIGHT),
-        Card(suit=Suit.DIAMOND, rank=Rank.NINE),
-        Card(suit=Suit.DIAMOND, rank=Rank.TEN),
-        Card(suit=Suit.DIAMOND, rank=Rank.KING),
-        Card(suit=Suit.DIAMOND, rank=Rank.QUEEN),
-        Card(suit=Suit.DIAMOND, rank=Rank.JACK),
-        # Club
-        Card(suit=Suit.CLUB, rank=Rank.ACE),
-        Card(suit=Suit.CLUB, rank=Rank.TWO),
-        Card(suit=Suit.CLUB, rank=Rank.THREE),
-        Card(suit=Suit.CLUB, rank=Rank.FOUR),
-        Card(suit=Suit.CLUB, rank=Rank.FIVE),
-        Card(suit=Suit.CLUB, rank=Rank.SIX),
-        Card(suit=Suit.CLUB, rank=Rank.SEVEN),
-        Card(suit=Suit.CLUB, rank=Rank.EIGHT),
-        Card(suit=Suit.CLUB, rank=Rank.NINE),
-        Card(suit=Suit.CLUB, rank=Rank.TEN),
-        Card(suit=Suit.CLUB, rank=Rank.KING),
-        Card(suit=Suit.CLUB, rank=Rank.QUEEN),
-        Card(suit=Suit.CLUB, rank=Rank.JACK),
-    ]
+    list_52_cards = LIST_OF_52_CARDS
 
     deck = Deck()
 
